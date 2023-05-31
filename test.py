2eac6e802351efa8b9f28039add44fce234bdb39
antoniatsv <atsvetanova95@gmail.com>
2023-05-31 12:01:13 +0100

    test deleted

--- test.py
+++
@@ -1,9 +0,0 @@
-"""HelloWorld"""
-
-import numpy as np
-
-def generate_x_fnc(N) -> np.ndarray:
-    X_1 = np.random.normal(loc = 1, size = N)
-    X_2 = np.random.normal(loc = 1, size = N) #loc (mean) should be higher because we want X to be distributed around positive numbers
-    
-    return X_1, X_2
